2f4ad1abcf1afcfd0bd4b010d140a30726a3c67b
webdev618 <webdev618@gmail.com>
2018-09-17 12:05:02 -0700

    1.4) add new type of post "Films".

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -22,3 +22,40 @@ function unite_child_css() {
     wp_enqueue_style( 'unite-child' );
 }
 
+/*
+ * Register "Films" post type.
+ */
+add_action( 'init', 'unite_child_register_post_types' );
+
+function unite_child_register_post_types() {
+    register_post_type(
+        'film',
+        array(
+            'labels' => unite_child_get_post_labels( __( 'Film', 'unite-child' ), __( 'Films', 'unite-child' ) ),
+            'exclude_from_search' => false,
+            'has_archive' => true,
+            'public' => true,
+            'rewrite' => array( 'slug' => 'film' ),
+            'supports' => array( 'title', 'editor' ),
+            'can_export' => true,
+            'show_in_nav_menus' => true
+        )
+    );
+}
+
+function unite_child_get_post_labels( $singular_name, $name ) {
+    return array(
+        'name' => $name,
+        'singular_name' => $singular_name,
+        'add_new' => __( 'Add New', 'unite-child' ),
+        'add_new_item' => sprintf( __( 'Add New %s', 'unite-child' ), $singular_name ),
+        'edit_item' => sprintf( __( 'Edit %s', 'unite-child' ), $singular_name ),
+        'new_item' => sprintf( __( 'New %s', 'unite-child' ), $singular_name ),
+        'view_item' => sprintf( __( 'View %s', 'unite-child' ), $singular_name ),
+        'search_items' => sprintf( __( 'Search %s', 'unite-child' ), $name ),
+        'not_found' => sprintf( __( 'No %s found', 'unite-child' ), $name ),
+        'not_found_in_trash' => sprintf( __( 'No %s found in Trash', 'unite-child' ), $name ),
+        'parent_item_colon' => ''
+    );
+}
+
