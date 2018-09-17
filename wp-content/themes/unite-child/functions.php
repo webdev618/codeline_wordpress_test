b6c87819a6291c72278158dddcb6b28282f46f06
webdev618 <webdev618@gmail.com>
2018-09-17 12:05:54 -0700

    1.5) Add following taxonimies to films: Genre, Country, Year and Actors

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -59,3 +59,75 @@ function unite_child_get_post_labels( $singular_name, $name ) {
     );
 }
 
+/*
+ * Register "Genre", "Country, "Year" and "Actors" taxonomies in "Films" post type.
+ */
+add_action( 'init', 'unite_child_register_taxonomies' );
+
+function unite_child_register_taxonomies() {
+    register_taxonomy(
+        'film_genre',
+        'film',
+        array(
+            'hierarchical' => true,
+            'show_in_nav_menus' => true,
+            'labels' => unite_child_get_taxonomy_labels( __( 'Genre', 'unite-child' ), __( 'Genres', 'unite-child' ) ),
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'film_genre' )
+        )
+    );
+
+    register_taxonomy(
+        'film_country',
+        'film',
+        array(
+            'hierarchical' => true,
+            'show_in_nav_menus' => true,
+            'labels' => unite_child_get_taxonomy_labels( __( 'Country', 'unite-child' ), __( 'Countries', 'unite-child' ) ),
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'film_country' )
+        )
+    );
+
+    register_taxonomy(
+        'film_year',
+        'film',
+        array(
+            'hierarchical' => true,
+            'show_in_nav_menus' => true,
+            'labels' => unite_child_get_taxonomy_labels( __( 'Year', 'unite-child' ), __( 'Years', 'unite-child' ) ),
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'film_year' )
+        )
+    );
+
+    register_taxonomy(
+        'film_actor',
+        'film',
+        array(
+            'hierarchical' => true,
+            'show_in_nav_menus' => true,
+            'labels' => unite_child_get_taxonomy_labels( __( 'Actor', 'unite-child' ), __( 'Actors', 'unite-child' ) ),
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'film_actor' )
+        )
+    );
+}
+
+function unite_child_get_taxonomy_labels( $singular_name, $name ) {
+    return array(
+        'name' => $name,
+        'singular_name' => $singular_name,
+        'search_items' => sprintf( __( 'Search %s', 'unite-child' ), $name ),
+        'all_items' => sprintf( __( 'All %s', 'unite-child' ), $name ),
+        'parent_item' => sprintf( __( 'Parent %s', 'unite-child' ), $singular_name ),
+        'parent_item_colon' => sprintf( __( 'Parent %s:', 'unite-child' ), $singular_name ),
+        'edit_item' => sprintf( __( 'Edit %', 'unite-child' ), $singular_name ),
+        'update_item' => sprintf( __( 'Update %s', 'unite-child' ), $singular_name ),
+        'add_new_item' => sprintf( __( 'Add New %s', 'unite-child' ), $singular_name ),
+        'new_item_name' => sprintf( __( 'New %s Name', 'unite-child' ), $singular_name ),
+        'not_found' => sprintf( __( 'No %s found', 'unite-child' ), $name ),
+        'menu_name' => $name,
+    );
+}
+
